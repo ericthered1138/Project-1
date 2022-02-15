7fa65d33a6d42e620adcd0afc5da22b352dee462
ericthered1138 <ericjennings955@gmail.com>
2022-02-15 16:32:45 -0600

    punched up the reimbursement dao layer

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,9 +3,6 @@
 0custom_exceptions/employee_could_not_be_found.py,6\9\6983d656dfd85a01d13953b14af64d3e4cb05be0
 ;
 records.log,c\f\cf429c79649736f8bdaec7f62f49f7cd74d8ccfc
-:
-
-schema.sql,2\9\29b783fbfb399c04c04bfdf2e93fe4096efaae81
 l
 <service_layer/implementation_classes/employee_service_imp.py,4\0\407ca53147a5016dd5f8dc0d92ef636ebe4b6388
 q
@@ -14,11 +11,7 @@ G
 e2etests/environment.py,0\9\09dcc01444e8d02398efa54a3373e19df788b48a
 K
 util/database_connection.py,8\c\8c326ba587a4284574861a310da04caa55f66c66
-l
-<data_access_layer/implementation_classes/employee_dao_imp.py,d\8\d8e3392751c43637ab16dc1a569c2c556a00d5df
 V
 &util/database_generator_for_testing.py,9\d\9dddd1ceae5c96269d0cfcab3f1729810d95ecdd
-m
-=tests_for_app/test_for_data_access_layer/test_employee_dao.py,c\3\c359961d69721c1f611c85e0c18a90c6d6beb658
 D
 entities/employee.py,1\5\15057c7321d0ade21a1bac4f6ad483dab9c0d2eb
--- data_access_layer/implementation_classes/reimbursement_dao_imp.py
+++ data_access_layer/implementation_classes/reimbursement_dao_imp.py
@@ -1,30 +1,19 @@
+from custom_exceptions.employee_could_not_be_found import EmployeeCouldNotBeFound
 from custom_exceptions.reimbursement_could_not_be_found import ReimbursementCouldNotBeFound
-from datetime import date, datetime
 
+from custom_exceptions.that_reimbursement_is_not_pending import ThatReimbursementIsNotPending
 from data_access_layer.abstract_classes.reimbursement_dao_abstract import ReimbursementDAO
 from entities.reimbursement import Reimbursement
 from util.database_connection import connection
 
 
 class ReimbursementDAOImp(ReimbursementDAO):
-    def __init__(self, reimbursement_counter=1000):
-        self.reimbursement_id_counter = self.max_creator(reimbursement_counter) + 1
 
-    @staticmethod
-    def max_creator(counter):
-        """Static Method sets the new customer id counter by finding the highest."""
-        cursor = connection.cursor()
-        sql = "select max(reimbursement_id) as highest_reimbursement_id from reimbursement_table"
-        cursor.execute(sql)
-        highest = cursor.fetchone()
-        if highest[0] is None:  # In case the table is empty.
-            return counter
-        return max(highest[0], counter)
-
-    def get_reimbursement(self, reimbursement: Reimbursement) -> Reimbursement:
+    def get_reimbursement(self, reimbursement_id: int) -> Reimbursement:
         """To get a reimbursement."""
+        sql = "select * from reimbursement_table where reimbursement_id = %(reimbursement_id)s;"
         cursor = connection.cursor()
-        cursor.execute(f"select * from reimbursement_table where reimbursement_id = {reimbursement.reimbursement_id}")
+        cursor.execute(sql, {"reimbursement_id": reimbursement_id})
         reimbursement_record = cursor.fetchone()
 
         # Check to see if there is a record otherwise raise an exception.
@@ -36,38 +25,56 @@ class ReimbursementDAOImp(ReimbursementDAO):
 
     def create_reimbursement(self, reimbursement: Reimbursement) -> Reimbursement:
         """For employees, to create reimbursements."""
-        reimbursement.reimbursement_id = self.reimbursement_id_counter
-        self.reimbursement_id_counter += 1
-
-        sql = f"insert into reimbursement_table (reimbursement_id, employee_id, reimbursement_date, amount, " \
-              f"reason, approval, manager_comment) " \
-              f"values ('{reimbursement.reimbursement_id}', '{reimbursement.employee_id}', " \
-              f"'{datetime.now()}', '{reimbursement.amount}', '{reimbursement.reason}', " \
-              f"'{reimbursement.if_approved}', '{reimbursement.manager_comment}');"
-
+        # Check if the employee actually exists.
         cursor = connection.cursor()
-        cursor.execute(sql)
+        sql = "select * from employee_table where employee_id = %(employee_id)s;"
+        cursor.execute(sql, {"employee_id": reimbursement.employee_id})
+        employee_record = cursor.fetchone()
+        if not employee_record:
+            raise EmployeeCouldNotBeFound('Employee could not be found.')
+
+        # Create the reimbursement.
+        sql = "insert into reimbursement_table (reimbursement_id, employee_id, amount, reason) " \
+              "values (default, %(employee_id)s, %(amount)s, %(reason)s) returning reimbursement_id;"
+
+        cursor.execute(sql, {"employee_id": reimbursement.employee_id,
+                             "amount": reimbursement.amount,
+                             "reason": reimbursement.reason
+                             })
         connection.commit()
-        return self.get_reimbursement(reimbursement)
+        reimbursement_id = cursor.fetchone()[0]
+        return self.get_reimbursement(reimbursement_id)
 
     def approve_reimbursement(self, reimbursement: Reimbursement) -> Reimbursement:
         """For managers, to approve a reimbursement."""
-        sql = f"update reimbursement_table set approval = 'yes' " \
-              f"where reimbursement_id = {reimbursement.reimbursement_id};" \
-              f"update reimbursement_table set manager_comment = '{reimbursement.manager_comment}' " \
-              f"where reimbursement_id = {reimbursement.reimbursement_id}"
-        cursor = connection.cursor()
-        cursor.execute(sql)
-        connection.commit()
-        return self.get_reimbursement(reimbursement)
 
-    def disapprove_reimbursement(self, reimbursement: Reimbursement) -> Reimbursement:
-        """For managers, to disapprove a reimbursement."""
-        sql = f"update reimbursement_table set approval = 'no' " \
-              f"where reimbursement_id = {reimbursement.reimbursement_id};" \
-              f"update reimbursement_table set manager_comment = '{reimbursement.manager_comment}' " \
-              f"where reimbursement_id = {reimbursement.reimbursement_id}"
+        # Check the reimbursement in the database to make sure that it exists and is pending.
+        checked_reimbursement = self.get_reimbursement(reimbursement.reimbursement_id)
+        if checked_reimbursement.if_approved != 'pending':
+            raise ThatReimbursementIsNotPending('That reimbursement is not pending.')
+
+        # Approve the reimbursement.
+        sql = f"update reimbursement_table set approval = 'yes', manager_comment = %(manager_comment)s " \
+              f"where reimbursement_id = %(reimbursement_id)s;"
         cursor = connection.cursor()
-        cursor.execute(sql)
+        cursor.execute(sql, {"manager_comment": reimbursement.manager_comment,
+                             "reimbursement_id": reimbursement.reimbursement_id})
         connection.commit()
-        return self.get_reimbursement(reimbursement)
+        return self.get_reimbursement(reimbursement.reimbursement_id)
+
+    def deny_reimbursement(self, reimbursement: Reimbursement) -> Reimbursement:
+        """For managers, to disapprove a reimbursement."""
+
+        # Check the reimbursement in the database to make sure that it exists and is pending.
+        checked_reimbursement = self.get_reimbursement(reimbursement.reimbursement_id)
+        if checked_reimbursement.if_approved != 'pending':
+            raise ThatReimbursementIsNotPending('That reimbursement is not pending.')
+
+        # Deny the reimbursement.
+        sql = f"update reimbursement_table set approval = 'no', manager_comment = %(manager_comment)s " \
+              f"where reimbursement_id = %(reimbursement_id)s;"
+        cursor = connection.cursor()
+        cursor.execute(sql, {"manager_comment": reimbursement.manager_comment,
+                             "reimbursement_id": reimbursement.reimbursement_id})
+        connection.commit()
+        return self.get_reimbursement(reimbursement.reimbursement_id)
