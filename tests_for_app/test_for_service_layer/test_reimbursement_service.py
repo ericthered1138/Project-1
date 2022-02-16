a77db81d8cf87f4629bf982e57b12948a40b4f8f
ericthered1138 <ericjennings955@gmail.com>
2022-02-16 10:03:14 -0600

    finished tests for service layer

--- tests_for_app/test_for_service_layer/test_reimbursement_service.py
+++ tests_for_app/test_for_service_layer/test_reimbursement_service.py
@@ -1,3 +1,5 @@
+from custom_exceptions.invalid_reimbursement import InvalidReimbursement
+from custom_exceptions.invalid_reimbursement_id import InvalidReimbursementID
 from data_access_layer.implementation_classes.reimbursement_dao_imp import ReimbursementDAOImp
 from service_layer.implementation_classes.reimbursement_service_imp import ReimbursementServiceImp
 from entities.reimbursement import Reimbursement
@@ -9,17 +11,57 @@ reimbursement_service = ReimbursementServiceImp(reimbursement_dao)
 
 def test_service_create_reimbursement_success():
     reimbursement_dao.create_reimbursement = MagicMock(return_value=True)
+    test_reimbursement = Reimbursement(employee_id=12345, amount=42.00, reason="some Reason")
+    boolean = reimbursement_service.service_create_reimbursement(test_reimbursement)
+    assert boolean is True
+
+
+def test_service_create_reimbursement_failure():
+    reimbursement_dao.create_reimbursement = MagicMock(return_value=False)
     test_reimbursement = Reimbursement()
-    reimbursement_service.service_create_reimbursement(test_reimbursement)
+    try:
+        reimbursement_service.service_create_reimbursement(test_reimbursement)
+        assert False
+    except InvalidReimbursement as e:
+        print(e)
+        assert str(e) == 'That reimbursement is not valid.'
 
 
 def test_service_approve_reimbursement_success():
     """Check to see if the reimbursement is in the database, then forward to the data access layer."""
+    reimbursement_dao.approve_reimbursement = MagicMock(return_value=True)
     test_reimbursement = Reimbursement(12345)
-    reimbursement_service.service_approve_reimbursement(test_reimbursement)
+    boolean = reimbursement_service.service_approve_reimbursement(test_reimbursement)
+    assert boolean is True
 
 
-def test_service_disapprove_reimbursement():
+def test_service_approve_reimbursement_failure():
     """Check to see if the reimbursement is in the database, then forward to the data access layer."""
+    reimbursement_dao.approve_reimbursement = MagicMock(return_value=False)
+    test_reimbursement = Reimbursement()
+    try:
+        reimbursement_service.service_approve_reimbursement(test_reimbursement)
+        assert False
+    except InvalidReimbursementID as e:
+        print(e)
+        assert str(e) == 'That reimbursement id is not valid.'
+
+
+def test_service_deny_reimbursement_success():
+    """Check to see if the reimbursement is in the database, then forward to the data access layer."""
+    reimbursement_dao.deny_reimbursement = MagicMock(return_value=True)
     test_reimbursement = Reimbursement(12345)
-    reimbursement_service.service_deny_reimbursement(test_reimbursement)
+    boolean = reimbursement_service.service_deny_reimbursement(test_reimbursement)
+    assert boolean is True
+    
+
+def test_service_deny_reimbursement_failure():
+    """Check to see if the reimbursement is in the database, then forward to the data access layer."""
+    reimbursement_dao.approve_reimbursement = MagicMock(return_value=False)
+    test_reimbursement = Reimbursement()
+    try:
+        reimbursement_service.service_deny_reimbursement(test_reimbursement)
+        assert False
+    except InvalidReimbursementID as e:
+        print(e)
+        assert str(e) == 'That reimbursement id is not valid.'
